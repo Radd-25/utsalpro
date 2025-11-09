f4fa203a55b95372c7f6d8a863caa3f6416f96bb
Raddin Pratama Rachmat <r4dd1nk4sep@gmail.com>
2025-11-09 17:33:04 +0700

    selesai mengerjakan.

---
+++ variabel.java
@@ -0,0 +1,20 @@
+public class variabel {
+    public static void main(String[] args) {
+        String nama = "Raddin Pratama Rachmat";
+        String ttl = "Bogor, 25 Mei 2007";
+        String jk = "Laki-laki";
+        String alamat = "Jl. Raya Tajur No. 123, Bogor";
+        long nohp = 6281234567890L;
+        int penghasilan = 15000000;
+
+
+        System.out.println("Assalamu'alaikum");
+        System.out.println("Izinkan saya memperkenalkan diri");
+        System.out.println("Nama Lengkap           : " + nama);
+        System.out.println("Tempat & Tanggal Lahir : " + ttl);
+        System.out.println("Jenis Kelamin          : " + jk);
+        System.out.println("Alamat Lengkap         : " + alamat);
+        System.out.println("No  HP                 : " + nohp);
+        System.out.println("Penghasilan Sebulan    : " + penghasilan);
+    }
+}
